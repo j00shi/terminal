597b4009307b5b74553e60d98934162ec5f8bfb6
Joshi <joshua.albrecht@mail.de>
2026-09-18 21:19:52 +0200

    Fish no longer runs in VSCodium

--- configs/shell/config.fish
+++ configs/shell/config.fish
@@ -2,6 +2,11 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 
 # overwrite greeting
 function fish_greeting
+    # Doesn't show the greeting inside VSCodium's integrated terminal
+    # Alternatively use the other line to only show the greeting in Konsole
+    # not set -q KONSOLE_VERSION && return
+    string match -q 'vscode' "$TERM_PROGRAM" && return
+
     # This chooses a random text file from the specified directory
     set -l art_dir "$HOME/Code/terminal/ascii-art/generated/"   # <-- Set the correct path!!
     set -l files $art_dir/*.txt
